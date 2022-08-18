27db659f34e52d5eac74e56ead792094a07062f9
Alan Jack <60137518+aldojack@users.noreply.github.com>
2022-08-18 10:20:00 +0100

    Added function to generate new numbers and re-roll function for button

--- tenzies-game/src/App.jsx
+++ tenzies-game/src/App.jsx
@@ -1,12 +1,38 @@
 import { useState } from 'react'
-import Dice from './components/Dice'
+import Die from './components/Die'
 import './App.css'
 
 function App() {
-const [dice, setDice] = useState(["1","2","3","4","5","6","2","3","4","5"])
+  const [dice, setDice] = useState(allNewDice())
+
+  const diceElements = dice.map((die, index) => {
+    return (
+        <Die className='dice' key={index} value={die.value} />
+    )
+})
+
+  function allNewDice() {
+    let diceArray = Array.apply(0, Array(10))
+      .map(() => {
+        return {
+          value: Math.ceil(Math.random() * 6),
+          isHeld: false
+        }
+      })
+    return diceArray
+  }
+
+  function rollDice() {
+    setDice(allNewDice())
+  }
+
+
   return (
     <main>
-      <Dice dice={dice} />
+      <div className='dice-container'>
+        {diceElements}
+      </div>
+      <button className='roll-btn' onClick={rollDice}>Roll</button>
     </main>
   )
 }
