ae62285c9d0013ae4971800ca322aab74b237f51
Alan Jack <60137518+aldojack@users.noreply.github.com>
2022-08-18 12:53:45 +0100

    Game instructions added and styling to button click and hold function added

--- tenzies-game/src/App.jsx
+++ tenzies-game/src/App.jsx
@@ -6,31 +6,52 @@ import {nanoid} from 'nanoid'
 function App() {
   const [dice, setDice] = useState(allNewDice())
 
-  const diceElements = dice.map((die) => {
-    return (
-        <Die className='dice' key={die.id} value={die.value} />
-    )
-})
+  function generateNewDie() {
+    return {
+      id: nanoid(),
+      value: Math.ceil(Math.random() * 6),
+      isHeld: false
+    }
+  }
 
   function allNewDice() {
+    
     let diceArray = Array.apply(0, Array(10))
       .map(() => {
-        return {
-          id: nanoid(),
-          value: Math.ceil(Math.random() * 6),
-          isHeld: false
-        }
+        return generateNewDie()
       })
     return diceArray
   }
 
   function rollDice() {
-    setDice(allNewDice())
+    setDice(prevState => {
+      return prevState.map(dice => {
+        return dice.isHeld ?
+          { ...dice } : generateNewDie()
+      })
+    })
+
+    // setDice(allNewDice())
   }
 
+  function holdDie(id) {
+    setDice(prevState => {
+      return prevState.map(die => {
+        return die.id === id ? {...die, isHeld: !die.isHeld} : die
+      })
+    })
+  }
+
+  const diceElements = dice.map((die) => {
+    return (
+        <Die className='dice' key={die.id} {...die} handleClick={() => holdDie(die.id)} />
+    )
+  })
 
   return (
     <main>
+      <h1 className='title'>Tenzies</h1>
+      <p className='instructions'>Roll untill all dice are the same.  Click each die to hold at its current value between rolls.  </p>
       <div className='dice-container'>
         {diceElements}
       </div>
--- tenzies-game/src/components/Die.jsx
+++ tenzies-game/src/components/Die.jsx
@@ -1,8 +1,12 @@
 import './Die.css'
 
 export default function Die(props) {
-    
+    console.log(props)
+    const styles = {
+        backgroundColor: props.isHeld ? "#59E391" : "#ffffff",
+        boxShadow: props.isHeld ? "inset 5px 5px 10px -3px rgba(0, 0, 0, 0.7)" : "",
+    }
     return (
-        <div className='dice'>{props.value}</div>
+        <div className='dice' style={styles} onClick={props.handleClick}>{props.value}</div>
     )
 }
